6c14b938f57383513ee2945f8f286855efc4c6df
tilerlgt <liguitong@aliyun.com>
2018-08-19 15:14:13 +0800

    add password validate method

--- login/urls.py
+++ login/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index,name='index')
+    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
+    path('validateuser', views.validateuser, name='validate')
 ]
--- login/views.py
+++ login/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import  render
+from django.shortcuts import redirect
 
 def index(request):
     return render(request,'login/index.html')
+def validateuser(request):
+    email = request.POST.get("email")
+    print(email)
+    password = request.POST.get('password')
+    print(password)
+    if password !='123':
+        return redirect('%s?next=%s' % ('index', 'login/validateuser'))
+    return HttpResponse("Hello,you login in to this site!")
