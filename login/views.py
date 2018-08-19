a37562bbe9f9472032d0443cd6da5c4c103047b8
tilerlgt <liguitong@aliyun.com>
2018-08-19 14:40:09 +0800

    add asset of login

--- login/views.py
+++ login/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import  render
+
 def index(request):
-    return HttpResponse("Hello,lgt ,you login in!")
+    return render(request,'login/index.html')
